daaf5d7fa0408fcd050eacef5229c45ea31a8a0a
Eric Smallwood <smallwood.eric@ymail.com>
2022-06-03 14:55:26 -0500

    remove unused code

--- namegen.js
+++ namegen.js
@@ -1,13 +1,4 @@
 function namegen(count) {
-	let vowels = {
-		'1': ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"],
-		'2': ["a", "e", "o", "u", "y"],
-		'3': ["br", "cr", "dr", "fr", "gr", "pr", "str", "tr", "bl", "cl", "fl", "gl", "pl", "sl", "sc", "sk", "sm", "sn", "sp", "ss", "st", "sw", "ch", "sh", "th", "wh"],
-		'4': ["ae", "ai", "ao", "au", "a", "ay", "ea", "ei", "eo", "eu", "e", "ey", "ua", "ue", "ui", "uo", "u", "uy", "ia", "ie", "iu", "io", "iy", "oa", "oe", "ou", "oi", "o", "oy"],
-		'5': ["turn", "ter", "nus", "rus", "tania", "hiri", "hines", "gawa", "nides", "carro", "rilia", "stea", "lia", "lea", "ria", "nov", "phus", "mia", "nerth", "wei", "ruta", "tov", "zuno", "vis", "lara", "nia", "liv", "tera", "gantu", "yama", "tune", "ter", "nus", "cury", "bos", "pra", "thea", "nope", "tis", "clite"],
-		'6': ["una", "ion", "iea", "iri", "illes", "ides", "agua", "olla", "inda", "eshan", "oria", "ilia", "erth", "arth", "orth", "oth", "illon", "ichi", "ov", "arvis", "ara", "ars", "yke", "yria", "onoe", "ippe", "osie", "one", "ore", "ade", "adus", "urn", "ypso", "ora", "iuq", "orix", "apus", "ion", "eon", "eron", "ao", "omia"]
-	};
-
 	let components = {
 		beginsWithCEndsWithV: ["tania", "hiri","gawa", "carro", "rilia", "stea", "lia", "lea", "ria", "mia", "wei", "ruta","zuno", "lara", "nia", "tera", "gantu", "yama", "tune", "cury", "pra", "thea", "clite"],
 		beginsWithCEndsWithC: ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "br", "cr", "dr", "fr", "gr", "pr", "str", "tr", "bl", "cl", "fl", "gl", "pl", "sl", "sc", "sk", "sm", "sn", "sp", "ss", "st", "sw", "ch", "sh", "th", "wh","turn", "ter", "nus", "rus","hines", "nides", "nov", "phus", "nerth", "vis", "liv", "ter", "nus", "bos", "tis"],
@@ -45,26 +36,10 @@ function namegen(count) {
 
 		return name.join('');
 	}
-	let mtx = [
-		[1, 2, 5],
-		[2, 3, 6],
-		[3, 4, 5],
-		[4, 3, 6],
-		[3, 4, 2, 5],
-		[2, 1, 3, 6],
-		[3, 4, 2, 5],
-		[4, 3, 1, 6],
-		[3, 4, 1, 4, 5],
-		[4, 1, 4, 3, 6]
-	];
+
 	let names = [];
 	for (let i = 0;  i < count; i++) {
 		let name = buildName();
-		// let comp = mtx[i % mtx.length];
-		// for (let j=0; j < comp.length; j++) {
-		// 	let randomCompIndex = Math.floor(Math.random() * vowels[comp[j]].length);
-		// 	name += vowels[comp[j]][randomCompIndex];
-		// }
 
 		// 1 in 10 chance to be numbered
 		let isNumbered =  !Boolean(Math.floor(Math.random() * 10));
